e25d4d6558f3635a6da40aa9ca12c769d422336e
x23062 <x23062xx@aitech.ac.jp>
2025-07-19 11:39:12 +0900

    SearchScreenReturnButton

--- frontend/src/app/employee/page.tsx
+++ frontend/src/app/employee/page.tsx
@@ -1,10 +1,12 @@
 import { EmployeeDetailsContainer } from "@/components/EmployeeDetailsContainer";
 import { GlobalContainer } from "@/components/GlobalContainer";
 import { Suspense } from 'react';
+import { BackButton } from "@/components/BackButton";
 
 export default function EmployeePage() {
   return (
     <GlobalContainer>
+      <BackButton />
       { /* Mark EmployeeDetailsContainer as CSR */ }
       <Suspense>
         <EmployeeDetailsContainer />
